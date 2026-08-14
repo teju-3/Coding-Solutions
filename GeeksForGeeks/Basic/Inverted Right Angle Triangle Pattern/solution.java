dcc9c8e6e13cd180162514a8ae54bf2da54f6941
Tejaswini S <tejaswinistha@gmail.com>
2026-08-14 20:42:39 +0530

    [Syncode] GeeksForGeeks/Basic/Inverted Right Angle Triangle Pattern

---
+++ GeeksForGeeks/Basic/Inverted Right Angle Triangle Pattern/solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+          for(int i=n;i>=1;i--){
+        for(int j=0;j<i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+
+        sc.close();
+    }
+}
