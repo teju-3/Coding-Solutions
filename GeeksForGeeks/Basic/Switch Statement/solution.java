e76f8b180fa33b3775c0d5633404c17ead3d5346
Tejaswini S <tejaswinistha@gmail.com>
2026-08-14 20:51:56 +0530

    [Syncode] GeeksForGeeks/Basic/Switch Statement

---
+++ GeeksForGeeks/Basic/Switch Statement/solution.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        switch(n){
+            case 1:
+                System.out.print("One");
+                break;
+             case 2:
+                 System.out.print("Two");
+                break;
+             case 3:
+                System.out.print("Three");
+                break;
+             case 4:
+                 System.out.print("Four");
+                break;
+             case 5:
+                 System.out.print("Five");
+                break;
+             case 6:
+                 System.out.print("Six");
+                break;
+             case 7:
+               System.out.print("Seven");
+                break;
+             case 8:
+                System.out.print("Eight");
+                break;
+             case 9:
+                 System.out.print("Nine");
+                break;
+             default:
+               System.out.print("Unknown");
+                break;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 | 3 | [Learn to Comment](./GeeksForGeeks/Easy/Learn%20to%20Comment) | [GeeksForGeeks](https://www.geeksforgeeks.org/problems/learn-to-comment/1?page=1&category=Java&sortBy=submissions) | Easy | 14 Aug 2026 | 08:37 pm |
 | 4 | [Inverted Right Angle Triangle Pattern](./GeeksForGeeks/Basic/Inverted%20Right%20Angle%20Triangle%20Pattern) | [GeeksForGeeks](https://www.geeksforgeeks.org/problems/inverted-right-angletriangle-1605691171--104349/1?page=1&category=Java&sortBy=submissions) | Basic | 14 Aug 2026 | 08:42 pm |
 | 5 | [Swap The Numbers](./GeeksForGeeks/Basic/Swap%20The%20Numbers) | [GeeksForGeeks](https://www.geeksforgeeks.org/problems/swap-the-numbers/1?page=1&category=Java&sortBy=submissions) | Basic | 14 Aug 2026 | 08:43 pm |
+| 6 | [Switch Statement](./GeeksForGeeks/Basic/Switch%20Statement) | [GeeksForGeeks](https://www.geeksforgeeks.org/problems/switch-statement/1?page=1&category=Java&sortBy=submissions) | Basic | 14 Aug 2026 | 08:51 pm |
